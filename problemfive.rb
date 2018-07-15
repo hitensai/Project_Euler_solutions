0882d4a6bade56ad9a8ffee438ccf7bb1cfdbe49
hiten <hitensai279@gmail.com>
2018-07-14 21:17:48 -0500

    problemfive

---
+++ problemfive.rb
@@ -0,0 +1,20 @@
+def divisible(n)
+  20.downto(1) do |i|
+    if n % i != 0
+      return false
+    end
+  end  
+  return true
+end
+
+def solve
+  n = 20
+  while true
+    if divisible(n)
+      return n
+    else
+      n += 20
+    end  
+  end  
+end
+puts solve
